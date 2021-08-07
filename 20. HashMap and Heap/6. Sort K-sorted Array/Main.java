ac056e152c0bb22c2f3afe75a2a1b95a615629c3
Ravi Anand <ravianandfbg@gmail.com>
2021-08-07 20:46:42 +0530

    Heap

--- 20. HashMap and Heap/6. Sort K-sorted Array/Main.java
+++ 20. HashMap and Heap/6. Sort K-sorted Array/Main.java
@@ -19,9 +19,7 @@ public class Main {
           pq.add(arr[i]);
       }
       for(int i = k+1 ; i < n ; i++){
-          if(arr[i] > pq.peek()){
               pq.add(arr[i]);
-          }
       }
       while(pq.size() > 0){
           System.out.println(pq.remove());
@@ -29,3 +27,39 @@ public class Main {
    }
 
 }
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//    public static void main(String[] args) throws Exception {
+//       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//       int n = Integer.parseInt(br.readLine());
+//       int[] arr = new int[n];
+
+//       for (int i = 0; i < n; i++) {
+//          arr[i] = Integer.parseInt(br.readLine());
+//       }
+
+//       int k = Integer.parseInt(br.readLine());
+//       // write your code here
+//       PriorityQueue<Integer> pq = new PriorityQueue<>();
+//       for(int i = 0 ; i <= k ; i++){
+//           pq.add(arr[i]);
+//       }
+//       for(int i = k ; i < n ; i++){
+//           if(arr[i] > pq.peek()){
+//               pq.add(arr[i]);
+//           }
+//       }
+//       while(pq.size() > 0){
+//           System.out.println(pq.remove());
+//       }
+//    }
+
+// }

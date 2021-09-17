425f708601462e7e451384d265b714c20b9ceb6c
Ravi Anand <ravianandfbg@gmail.com>
2021-09-17 20:24:14 +0530

    code

---
+++ Level 2/2. Linked List/5. Unfold Of Linkedlist/Main.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Main {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    
+    public static ListNode reverse(ListNode head) {
+      
+        ListNode curr = head , prev = null;
+        while(curr != null){
+            ListNode nbr = curr.next;
+            curr.next = prev;
+            
+            prev = curr;
+            curr = nbr;
+        }
+        return prev;
+    }
+
+    public static void unfold(ListNode head) {
+        ListNode lh = new ListNode(-1); //dummy node for left part
+        ListNode lt = lh;
+        
+        ListNode rh = new ListNode(-1); //dummy node for right part
+        ListNode rt = rh;
+        
+        ListNode temp = head;
+        boolean flag = true;
+        
+        while(temp != null){
+            if(flag == true){
+                //add in left part
+                lt.next = temp;
+                lt = lt.next;
+            }
+            else{
+                //add in right part
+                rt.next = temp;
+                rt = rt.next;
+            }
+            flag = !flag; //tweak
+            temp = temp.next;
+            
+        }
+        
+        rt.next = null;
+        
+        rh = reverse(rh.next);
+        lt.next = rh;
+
+    }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = dummy.next;
+        unfold(head);
+        printList(head);
+    }
+}

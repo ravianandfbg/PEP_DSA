303c53ad535d668dfdde1b1f643b4e236185a22a
Ravi Anand <ravianandfbg@gmail.com>
2022-01-11 19:23:18 +0530

    code

---
+++ Level 2/6. Arrays & String/16. Partition Array Into Disjoint Intervals/Main.java
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+// time :O(N) , space O(N)
+
+// public static int partitionDisjoint(int[] arr) {
+//       // write your code here
+      
+//       // time : O(N)
+//       // space : O(N)
+      
+//       int[] lmax = new int[arr.length]; // lmax : left max
+//           int[] rmin = new int[arr.length + 1]; // rmin : right minimum
+          
+//           lmax[0] = arr[0];
+          
+//           for(int i = 1 ; i < arr.length ; i++){
+//               lmax[i] = Math.max(lmax[i - 1] , arr[i]);
+//           }
+          
+//           rmin[arr.length] = Integer.MAX_VALUE;
+          
+//           for(int i = arr.length - 1 ; i >= 0 ; i--){
+//               rmin[i] = Math.min(rmin[i + 1] , arr[i]);
+//           }
+          
+//           int chunks = 0;
+          
+//           for(int i = 0 ; i < arr.length ; i++){
+//               if(lmax[i] <= rmin[i + 1]){
+//                   return i + 1;
+//               }
+//           }
+//           return -1;
+//     }

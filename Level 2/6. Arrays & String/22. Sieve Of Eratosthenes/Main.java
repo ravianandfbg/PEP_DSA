d609a3cf791f11f1200f2191e16d976bf03eef34
Ravi Anand <ravianandfbg@gmail.com>
2022-01-15 13:02:42 +0530

    code

---
+++ Level 2/6. Arrays & String/22. Sieve Of Eratosthenes/Main.java
@@ -0,0 +1,34 @@
+// time : O(N*(log(log(N)))  , space : O(N)
+
+import java.util.*;
+
+public class Main {
+    
+    // ~~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~
+    public static void printPrimeUsingSieve(int n) {
+        // write your code here
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr,true); // initially mark all numbers true
+        
+        for(int k = 2 ; k * k <= n ; k++){
+            if(arr[k] == true){
+                for(int i = 2*k ; i <= n ; i += k){
+                    arr[i] = false;
+                }
+            }
+        }
+        for(int i = 2 ; i <= n ; i++){
+            if(arr[i] == true){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // ~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~
+    public static void main(String[] args) {   
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printPrimeUsingSieve(n);
+    }
+}
+

11eb06a87d1e8a832e3b5abd4328fae0e057a9fa
Ravi Anand <ravianandfbg@gmail.com>
2022-01-23 21:21:53 +0530

    code

--- Level 2/6. Arrays & String/33. Valid Palindrome 2/Main.java
+++ Level 2/6. Arrays & String/33. Valid Palindrome 2/Main.java
@@ -1,2 +1,33 @@
 // Two pointer
+// time : O(N)     space : O(1)
 
+class Main {
+      public boolean validPalindrome(String s) {
+          int i = 0;
+          int j = s.length() - 1;
+          
+          while(i < j){
+              if(s.charAt(i) == s.charAt(j)){
+                  i++;
+                  j--;
+              }
+              else{
+                  return isPalindromic(s , i+1 , j) || isPalindromic(s , i , j-1);
+              }
+          }
+          return true;
+      }
+      
+      public static boolean isPalindromic(String str , int i , int j){
+          while(i < j){
+              if(str.charAt(i) == str.charAt(j)){
+                  i++;
+                  j--;
+              }
+              else{
+                  return false;
+              }
+          }
+          return true;
+      }
+  }

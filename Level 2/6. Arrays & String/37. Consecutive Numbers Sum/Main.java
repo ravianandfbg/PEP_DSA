0492445baa058f537a0250aedde12ae7487867b8
Ravi Anand <ravianandfbg@gmail.com>
2022-01-24 14:06:19 +0530

    code

---
+++ Level 2/6. Arrays & String/37. Consecutive Numbers Sum/Main.java
@@ -0,0 +1,16 @@
+class Main {
+      public int consecutiveNumbersSum(int n) {
+          int count = 0;
+          
+          for(int k = 1 ; k*k + k <= 2*n ; k++){
+              
+              int num = n - (k * (k - 1) / 2);
+              
+              if(num % k == 0){
+                  // x will be a positive integer
+                  count++;
+              }
+          }
+          return count;
+      }
+  }

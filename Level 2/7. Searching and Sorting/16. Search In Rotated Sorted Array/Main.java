70ee1cb567547f7f295e3169aca463d99176cc04
Ravi Anand <ravianandfbg@gmail.com>
2022-02-03 13:09:58 +0530

    code

--- Level 2/7. Searching and Sorting/16. Search In Rotated Sorted Array/Main.java
+++ Level 2/7. Searching and Sorting/16. Search In Rotated Sorted Array/Main.java
@@ -3,28 +3,35 @@
 // Binary Search
 
 class Solution {
-      public int search(int[] nums, int target) {
-          int start = 0;
-          int end = nums.length - 1;
-          while (start <= end){
-              int mid = (start + end) / 2;
-              if (nums[mid] == target)
-                  return mid;
-          
-              if (nums[start] <= nums[mid]){
-                   if (target < nums[mid] && target >= nums[start]) 
-                      end = mid - 1;
-                   else
-                      start = mid + 1;
-              } 
-          
-              if (nums[mid] <= nums[end]){
-                  if (target > nums[mid] && target <= nums[end])
-                      start = mid + 1;
-                   else
-                      end = mid - 1;
-              }
-          }
-          return -1;
-      }
-  }
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while (lo <= hi){
+            int mid = (lo + hi) / 2;
+            
+            if (nums[mid] == target){
+                return mid;
+            }
+        
+            else if (nums[lo] <= nums[mid]){
+                // lo to mid is sorted
+                 if (nums[lo] <= target && target <= nums[mid]){
+                     hi = mid - 1;
+                 }
+                 else{
+                     lo = mid + 1;
+                 }
+            } 
+        
+            else if (nums[mid] <= nums[hi]){
+                // mid to hi is sorted
+                if (nums[mid] <= target && target <= nums[hi])
+                    lo = mid + 1;
+                 else
+                    hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

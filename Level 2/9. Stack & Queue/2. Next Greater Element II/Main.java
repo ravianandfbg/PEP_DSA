eb245cfe66cd03f92c15e7af49120316751c0e59
Ravi Anand <ravianandfbg@gmail.com>
2022-02-11 12:27:55 +0530

    code

--- Level 2/9. Stack & Queue/2. Next Greater Element II/Main.java
+++ Level 2/9. Stack & Queue/2. Next Greater Element II/Main.java
@@ -1,2 +1,32 @@
 // Time : O(N)          Space : O(N)
 
+class Solution {
+      public int[] nextGreaterElements(int[] nums) {
+          int n = nums.length;
+          int[] nge = new int[n]; // nge : nextgreater element
+          
+          Stack<Integer> st = new Stack<>();
+          
+          for(int i = n - 1 ; i >= 0 ; i--){
+              while(st.size() > 0 && st.peek() <= nums[i]){
+                  st.pop();
+              }
+              st.push(nums[i]);
+          }
+          
+          for(int i = n - 1 ; i >= 0 ; i--){
+              while(st.size() > 0 && st.peek() <= nums[i]){
+                  st.pop();
+              }
+              
+              if(st.size() == 0){
+                  nge[i] = -1;
+              }
+              else{
+                  nge[i] = st.peek();
+              }
+              st.push(nums[i]);
+          }
+          return nge;
+      }
+  }

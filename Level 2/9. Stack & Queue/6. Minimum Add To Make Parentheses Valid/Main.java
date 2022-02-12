a56bcf043c6bffad2d653f3a436d83312fb85b82
Ravi Anand <ravianandfbg@gmail.com>
2022-02-12 17:21:46 +0530

    code

---
+++ Level 2/9. Stack & Queue/6. Minimum Add To Make Parentheses Valid/Main.java
@@ -0,0 +1,25 @@
+// Time : O(N)    space : O(N)
+
+class Solution {
+      public int minAddToMakeValid(String s) {
+          Stack<Character> st = new Stack<>();
+          
+          for(int i = 0 ; i < s.length() ; i++){
+              char ch = s.charAt(i);
+              
+              if(ch == '('){
+                  st.push(ch);
+              }
+              else if(ch == ')'){
+                  // is '(' available for me
+                  if(st.size() > 0 && st.peek() == '('){
+                      st.pop();
+                  }
+                  else{
+                      st.push(ch);
+                  }
+              }
+          }
+          return st.size();
+      }
+  }

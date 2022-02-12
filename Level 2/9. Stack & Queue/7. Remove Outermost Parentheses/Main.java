37ba8866bc32dbfb69448b0b5680d08854206171
Ravi Anand <ravianandfbg@gmail.com>
2022-02-12 17:51:36 +0530

    code

--- Level 2/9. Stack & Queue/7. Remove Outermost Parentheses/Main.java
+++ Level 2/9. Stack & Queue/7. Remove Outermost Parentheses/Main.java
@@ -1,2 +1,27 @@
-// Time : O()           Space : O()
+// Time : O(N)           Space : O(N)
 
+class Solution {
+      public String removeOuterParentheses(String s) {
+          Stack<Character> st = new Stack<>();
+          StringBuilder sb = new StringBuilder("");
+          
+          for(int i = 0 ; i < s.length() ; i++){
+              char ch = s.charAt(i);
+              
+              if(ch == '('){
+                  if(st.size() != 0){
+                      sb.append('(');
+                  }
+                  st.push(ch);
+              }
+              else{
+                  st.pop();
+                  
+                  if(st.size() != 0){
+                      sb.append(')');
+                  }
+              }
+          }
+          return sb.toString();
+      }
+  }

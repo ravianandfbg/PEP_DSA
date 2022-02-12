7a614b39fc9caf3919923f0e9e5f92aaa81c447a
Ravi Anand <ravianandfbg@gmail.com>
2022-02-12 15:13:35 +0530

    code

--- Level 2/9. Stack & Queue/5. Validate Stack Sequences/Main.java
+++ Level 2/9. Stack & Queue/5. Validate Stack Sequences/Main.java
@@ -1,2 +1,30 @@
-// Time : O()           space : O()
+// Time : O(N)           space : O(1)
 
+// two pointers approach
+
+class Solution {
+      public boolean validateStackSequences(int[] pushed, int[] popped) {
+          int i = 0;
+          int j = 0;
+          int count = 0;
+          
+          Stack<Integer> st= new Stack<>();
+          int n = pushed.length;
+          
+          while(j < n){
+              if(st.size() > 0 && popped[j] == st.peek()){
+                  st.pop();
+                  j++;
+                  count++;
+              }
+              else if(i < n){
+                  st.push(pushed[i]);
+                  i++;
+              }
+              else{
+                  return false;
+              }
+          }
+          return count == n;
+      }
+  }

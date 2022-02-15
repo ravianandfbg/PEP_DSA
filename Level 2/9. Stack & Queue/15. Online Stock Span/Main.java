78577e89dd41d4d1c27407a22f833902871eb80c
Ravi Anand <ravianandfbg@gmail.com>
2022-02-15 21:44:35 +0530

    code

---
+++ Level 2/9. Stack & Queue/15. Online Stock Span/Main.java
@@ -0,0 +1,30 @@
+// Time : O(N)          Space : O(N)
+
+class StockSpanner {
+      ArrayList<Integer> list;
+      Stack<Integer> st;
+  
+      public StockSpanner() {
+          list = new ArrayList<>();
+          st = new Stack<>();
+      }
+      
+      public int next(int price) {
+          int idx = list.size();
+          
+          while(st.size() > 0 && list.get(st.peek()) <= price){
+              st.pop();
+          }
+          
+          int ans = 0;
+          if(st.size() == 0){
+              ans = idx + 1;
+          }
+          else{
+              ans = idx- st.peek();
+          }
+          st.push(idx);
+          list.add(price);
+          return ans;
+      }
+  }

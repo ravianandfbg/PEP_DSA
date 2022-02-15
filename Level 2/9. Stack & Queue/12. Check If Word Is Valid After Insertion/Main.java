bc62d3ae64071a82b6f7dd0b2f31435648f164ef
Ravi Anand <ravianandfbg@gmail.com>
2022-02-15 13:40:54 +0530

    code

--- Level 2/9. Stack & Queue/12. Check If Word Is Valid After Insertion/Main.java
+++ Level 2/9. Stack & Queue/12. Check If Word Is Valid After Insertion/Main.java
@@ -1,2 +1,25 @@
 // Time : O(N)          Space : O(N)
 
+class Solution {
+      public boolean isValid(String s) {
+          Stack<Character> st = new Stack<>();
+          
+          for(int i = 0 ; i < s.length() ; i++){
+              char ch = s.charAt(i);
+              
+              if(ch == 'a' || ch == 'b'){
+                  st.push(ch);
+              }
+              else{
+                  if(st.size() >= 2 && st.pop() == 'b' && st.pop() == 'a'){
+                      // no issue
+                  }
+                  else{
+                      return false;
+                  }
+              }
+          }
+          
+          return st.size() == 0;
+      }
+  }

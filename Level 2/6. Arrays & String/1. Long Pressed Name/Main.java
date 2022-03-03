f4401c09ca89229a793106c0b6c377e643f849a8
Ravi Anand <ravianandfbg@gmail.com>
2022-03-03 21:45:34 +0530

    code

--- Level 2/6. Arrays & String/1. Long Pressed Name/Main.java
+++ Level 2/6. Arrays & String/1. Long Pressed Name/Main.java
@@ -1,24 +1,6 @@
-import java.util.*;
- 
- import javax.sound.sampled.BooleanControl.Type;
- 
- public class Main {
- 	 public static void main(String[] args) {
- 
- 	 	 Scanner scn = new Scanner(System.in);
- 	 	 String name = scn.next();
- 	 	 String typed = scn.next();
- 	 	 System.out.println(isLongPressedName(name, typed));
- 
- 	 }
- 
- 	 // -----------------------------------------------------
- 	 // This is a functional problem. Only this function has to be written.
- 	 // This function takes as input 2 strings
- 	 // It should return boolean output.
- 	 public static boolean isLongPressedName(String name, String typed) {
- 	 	 // Write your code here
- 	 	  if(typed.length() < name.length()){
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        if(typed.length() < name.length()){
             return false;
         }
         
@@ -38,6 +20,5 @@ import java.util.*;
             }
         }
         return i == name.length();
- 	 }
- 
- }
+    }
+}

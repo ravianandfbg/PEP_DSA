004d0987c0e628930d4ee6418c5b6c1233a72454
Ravi Anand <ravianandfbg@gmail.com>
2022-04-17 10:21:19 +0530

    code

--- Level 2/6. Arrays & String/6. Product Of Array Except Itself Without Using Division Operator/Main.java
+++ Level 2/6. Arrays & String/6. Product Of Array Except Itself Without Using Division Operator/Main.java
@@ -11,7 +11,7 @@ public class Main {
         
         lp[0] = nums[0];
         
-        for(int i = 1 ; i <n ; i++){
+        for(int i = 1 ; i < n ; i++){
             lp[i] = lp[i - 1] * nums[i];
         }
         

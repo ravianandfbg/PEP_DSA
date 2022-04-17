bf2a96c2ccfa9ee874bb13c82c8f5289fd91a81e
Ravi Anand <ravianandfbg@gmail.com>
2022-04-17 20:46:10 +0530

    code

--- Level 2/6. Arrays & String/8. Majority Elements - I I/Main.java
+++ Level 2/6. Arrays & String/8. Majority Elements - I I/Main.java
@@ -1,11 +1,9 @@
-import java.util.*;
+// time:O(N)
+// space:O(1)
+// Boyer-Moore Majority Vote algorithm
 
-public class Main {
-
-    // ~~~~~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~~~~~
-    public static ArrayList<Integer> majorityElement2(int[] nums) {
-        // write your code here
-        
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
         int val1 = nums[0];
         int count1 = 1;
         
@@ -21,22 +19,23 @@ public class Main {
             }
             
             else{
-            if(count1 == 0){
-                val1 = nums[i];
-                count1 = 1;
+                if(count1 == 0){
+                    val1 = nums[i];
+                    count1 = 1;
+                }
+                else if(count2 == 0){
+                    val2 = nums[i];
+                    count2 = 1;
+                }
+                else{
+                    count1--;
+                    count2--;
+                }
             }
-            else if(count2 == 0){
-                val2 = nums[i];
-                count2 = 1;
-            }
-            else{
-                count1--;
-                count2--;
-            }
-          }
         }
         
         int freq1 = 0 , freq2 = 0;
+        
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] == val1){
                 freq1++;
@@ -45,6 +44,7 @@ public class Main {
                 freq2++;
             }
         }
+        
         ArrayList<Integer> ans = new ArrayList<>();
         
         if(freq1 > nums.length / 3){
@@ -53,20 +53,7 @@ public class Main {
         if(freq2 > nums.length / 3){
             ans.add(val2);
         }
+        
         return ans;
-    }
-
-    // ~~~~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~~~~~
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
-        }
-
-        ArrayList<Integer> res = majorityElement2(arr);
-        System.out.println(res);
     }
 }
--- Level 2/6. Arrays & String/9. Majority Element General/Main.java
+++ Level 2/6. Arrays & String/9. Majority Element General/Main.java
@@ -1,57 +1,45 @@
-// time:O(N)
-// space:O(1)
-// Boyer-Moore Majority Vote algorithm
+import java.util.*;
 
-
-class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int val1 = nums[0];
-        int count1 = 1;
-        
-        int val2 = 0;
-        int count2 = 0;
-        
-        for(int i = 1 ; i < nums.length ; i++){
-            if(nums[i] == val1){
-                count1++;
-            }
-            else if(nums[i] == val2){
-                count2++;
-            }
-            else{
-                if(count1 == 0){
-                    val1 = nums[i];
-                    count1 = 1;
-                }
-                else if(count2 == 0){
-                    val2 = nums[i];
-                    count2 = 1;
-                }
-                else{
-                    count1--;
-                    count2--;
-                }
-            }
-        }
-        int freq1 = 0;
-        int freq2 = 0;
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] == val1){
-                freq1++;
-            }
-            else if(nums[i] == val2){
-                freq2++;
-            }
-        }
+public class Main {
+    
+    //~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~
+    public static ArrayList<Integer> majorityElement(int[] arr, int k) {
+        // write yout code here
         ArrayList<Integer> ans = new ArrayList<>();
         
-        if(freq1 > nums.length / 3){
-            ans.add(val1);
+        // freq map
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int val : arr){
+            if(map.containsKey(val) == false){
+                map.put(val , 1);
+            }
+            else{
+                int nFreq = map.get(val) + 1; // nFreq : new frequency
+                map.put(val , nFreq);
+            }
         }
-        if(freq2 > nums.length / 3){
-            ans.add(val2);
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            
+            if(freq > arr.length / k){
+                ans.add(key);
+            }
         }
+        Collections.sort(ans);
         return ans;
+    }
+    
+    //~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        ArrayList<Integer> res = majorityElement(arr, k);
+        System.out.println(res);
     }
 }

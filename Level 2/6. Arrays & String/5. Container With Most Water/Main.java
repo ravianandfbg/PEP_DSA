8c5492a456aab993d052149d77beaa01cf4111d3
Ravi Anand <ravianandfbg@gmail.com>
2022-04-28 11:42:08 +0530

    code

--- Level 2/6. Arrays & String/5. Container With Most Water/Main.java
+++ Level 2/6. Arrays & String/5. Container With Most Water/Main.java
@@ -1,45 +1,28 @@
-import java.util.*;
+// // TWO POINTER
+// time : O(N)
 
-public class Main {
-
-    // ~~~~~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~~~~~
-
-    public static int mostWater(int[] height) {
-        // write your code here
+class Solution {
+    public int maxArea(int[] height) {
         int i = 0;
         int j = height.length - 1;
         
         int maxArea = 0;
-        while(i < j){
+        
+        while(i <= j){
             int width = j - i;
-            
             int area = width * Math.min(height[i] , height[j]);
             
             if(area > maxArea){
                 maxArea = area;
-            }
+                }
+            
             if(height[i] < height[j]){
-                i++;
+                    i++;
             }
             else{
                 j--;
             }
         }
         return maxArea;
-    }
-
-    // ~~~~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~~~~~
-
-    public static void main(String[] args) {
-        // Write your code here
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[] heights = new int[n];
-        for(int i = 0; i < n; i++) {
-            heights[i] = scn.nextInt();
-        }
-
-        int res = mostWater(heights);
-        System.out.println(res);
     }
 }

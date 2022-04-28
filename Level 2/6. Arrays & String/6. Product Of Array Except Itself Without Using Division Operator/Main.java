bfe208cc335fdf8da14982eb813cfd04abf86e02
Ravi Anand <ravianandfbg@gmail.com>
2022-04-28 11:57:47 +0530

    code

--- Level 2/6. Arrays & String/6. Product Of Array Except Itself Without Using Division Operator/Main.java
+++ Level 2/6. Arrays & String/6. Product Of Array Except Itself Without Using Division Operator/Main.java
@@ -1,13 +1,11 @@
-import java.util.*;
+// time : O(N)
+// space : O(N)
 
-public class Main {
-    
-    //~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~
-    public static int[] productExceptSelf(int[] nums) {
-        // write your code here
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] lp = new int[n]; // lp : left product
-        int[] rp = new int[n]; // rp : right product
+        int lp[] = new int[n]; // lp : left product
+        int rp[] = new int[n]; // rp : right product
         
         lp[0] = nums[0];
         
@@ -15,31 +13,19 @@ public class Main {
             lp[i] = lp[i - 1] * nums[i];
         }
         
-        rp[n - 1] = nums[n - 1];
+        rp[n-1] = nums[n-1];
         
         for(int i = n - 2 ; i >= 0 ; i--){
             rp[i] = nums[i] * rp[i + 1];
         }
         
         int[] ans = new int[n];
-        for(int i = 0 ; i < nums.length ; i++){
-            int pes = (i == 0 ? 1 : lp[i - 1]) * (i == n - 1 ? 1 : rp[i + 1]); // pes : product except self
+        
+        for(int i = 0 ; i < n ; i++){
+            int pes = (i == 0 ? 1 : lp[i - 1]) * (i == n - 1 ? 1 : rp[i + 1]); //pes : product except self
             ans[i] = pes;
         }
+        
         return ans;
-    }
-
-    //~~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
-        }
-        int[] res = productExceptSelf(arr);
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(res[i] + " ");
-        }
     }
 }

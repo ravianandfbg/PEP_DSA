167e85103f559747a69064d8d63080e3d5545083
Ravi Anand <ravianandfbg@gmail.com>
2022-04-30 23:03:35 +0530

    code

--- Level 2/6. Arrays & String/25. Merge Intervals/Main.java
+++ Level 2/6. Arrays & String/25. Merge Intervals/Main.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
+// T.C : O(NlogN) + O(N)
+// S.c : O(N)
 
 public class Main{
     public static int[][] mergeIntervals(int intervals[][]){
-        // write your code here
         ArrayList<int[]> ans = new ArrayList<>();
         
         Arrays.sort(intervals , (a,b) -> {
@@ -37,29 +35,5 @@ public class Main{
         res = ans.toArray(res);
         
         return res;
-    }
-    public static void main(String args[]){
-        Scanner scn = new Scanner(System.in);
-
-        // Input Format
-        int n = scn.nextInt();
-        int input[][] = new int[n][2];
-
-        for(int i = 0 ; i <  n ; i++){
-            int sp = scn.nextInt();
-            int ep = scn.nextInt();
-
-            input[i][0] = sp;
-            input[i][1] = ep;
-        }
-
-        // Output Format
-        int [][]output = mergeIntervals(input);
-
-        System.out.print("[");
-        for(int arr[] : output){
-            System.out.print(Arrays.toString(arr));
-        }
-        System.out.println("]");
     }
 }

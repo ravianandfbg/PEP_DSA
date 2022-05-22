66cf034504910a3ad9633b664cb0fa333d8aea2d
Ravi Anand <ravianandfbg@gmail.com>
2022-05-22 22:52:01 +0530

    code

--- Level 2/7. Searching and Sorting/3. Search A 2d Matrix-2/Main.java
+++ Level 2/7. Searching and Sorting/3. Search A 2d Matrix-2/Main.java
@@ -2,8 +2,11 @@
 
 class Solution {
       public boolean searchMatrix(int[][] matrix, int target) {
-           int i = 0, j = matrix[0].length - 1;
+           int i = 0;
+           int j = matrix[0].length - 1;
+
               while (i < matrix.length && j >= 0) {
+
                       if (matrix[i][j] == target) {
                           return true;
                       } 

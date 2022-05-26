26c557cd6eeebca97b92172672a6590d823cb915
Ravi Anand <ravianandfbg@gmail.com>
2022-05-26 18:25:10 +0530

    code

--- Level 2/7. Searching and Sorting/5. Find K Closest Elements/Main.java
+++ Level 2/7. Searching and Sorting/5. Find K Closest Elements/Main.java
@@ -51,3 +51,31 @@ class Solution {
           return ans;
       }
   }
+
+
+
+
+  // time : O(N)
+
+//   class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         int lo = 0;
+//         int hi = arr.length - 1;
+        
+//         while(hi - lo >= k){
+//             if(Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)){
+//                 lo++;
+//             }
+//             else{
+//                 hi--;
+//             }
+//         }
+        
+//         List<Integer> ans = new ArrayList<>();
+//         for(int i = lo ; i <= hi ; i++){
+//             ans.add(arr[i]);
+//         }
+        
+//         return ans;
+//     }
+// }
